ddce34e591fe607c9d331f74c047745fad92279d
EloiGry <eloi.grychta@gmail.com>
2022-08-03 17:57:20 +0200

    modif

---
+++ queries/getMenus.js
@@ -0,0 +1,46 @@
+import { gql } from "@apollo/client"
+
+export const GET_MENUS = gql `
+{
+  headerMenu: menuItems(where: {location: HCMS_MENU_HEADER, parentId: "0"}) {
+    edges {
+      node {
+        id
+        label
+        path
+        url
+        childItems {
+          edges {
+            node {
+              id
+              label
+              path
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+  footerMenu: menuItems(where: {location: HCMS_MENU_FOOTER, parentId: "0"}) {
+    edges {
+      node {
+        id
+        label
+        path
+        url
+        childItems {
+          edges {
+            node {
+              id
+              label
+              path
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+}
+` 
